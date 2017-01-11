c73fb666bf1ee7777cc2be205ec5cca61bd103d3
A. M. Douglas <me@amdouglas.com>
2017-01-11 16:32:49 +0000

    Update sw.js

--- editor/sw.js
+++ editor/sw.js
@@ -2,7 +2,7 @@
 
 self.importScripts('https://seoscribe.net/assets/js/serviceworker-cache-polyfill.js');
 
-const CACHE_VERSION = 21;
+const CACHE_VERSION = 22;
 const CURRENT_CACHES = { prefetch: 'seoscribe-v' + CACHE_VERSION };
 
 self.addEventListener('install', event => {
