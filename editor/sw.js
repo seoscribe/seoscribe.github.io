8c729a37d86bdaaad12320c6014510e73000fbae
A. M. Douglas <me@amdouglas.com>
2017-01-12 17:06:10 +0000

    Update sw.js

--- editor/sw.js
+++ editor/sw.js
@@ -1,8 +1,10 @@
 'use strict';
 
-self.importScripts('https://seoscribe.net/assets/js/serviceworker-cache-polyfill.js');
+// lets use ES6 because why not?
+// this polyfill is useless -- the ServiceWorker will not work in Chrome 40 due to arrow functions
+// self.importScripts('https://seoscribe.net/assets/js/serviceworker-cache-polyfill.js');
 
-const CACHE_VERSION = 2;
+const CACHE_VERSION = 3;
 const CURRENT_CACHES = { prefetch: 'editor-v' + CACHE_VERSION };
 
 self.addEventListener('install', event => {
