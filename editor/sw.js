f262ae39a199972b3296154c349e70533fe15afd
A. M. Douglas <me@amdouglas.com>
2017-01-12 17:07:27 +0000

    Update sw.js

--- editor/sw.js
+++ editor/sw.js
@@ -12,7 +12,6 @@ self.addEventListener('install', event => {
     'https://seoscribe.net/editor/',
     'https://seoscribe.net/assets/js/editor.js',
     'https://seoscribe.net/assets/js/editor-worker.js',
-    'https://seoscribe.net/favicon.ico',
     'https://seoscribe.net/manifest.json'
   ];
   event.waitUntil(
