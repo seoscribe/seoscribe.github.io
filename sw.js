1505ea0624aa263213015cb696cff9e74a91a18c
A. M. Douglas <me@amdouglas.com>
2017-02-08 20:30:03 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = 22;
+const CACHE_VERSION = 25;
 const CURRENT_CACHES = { prefetch: 'seoscribe-v' + CACHE_VERSION };
 
 self.addEventListener('install', event => {
